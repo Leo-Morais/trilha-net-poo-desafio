ff566cdf9bedc4a273882a439973757af668a430
Leo-Morais <leo.vgvm@gmail.com>
2022-11-08 17:21:01 -0300

    Desafio concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,19 +2,31 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+         public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+        private string Modelo { get; set; }
+
+        private string Imei { get; set; }
+
+        private int Memoria { get; set;}
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
+
+        
 
         public void ReceberLigacao()
         {
